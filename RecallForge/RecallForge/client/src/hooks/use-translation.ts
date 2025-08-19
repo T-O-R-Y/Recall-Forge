6dc51d57c8aa1826f7b5d694f21d1c4610bf116b
T-O-R-Y <minjae4137@naver.com>
2025-08-19 21:09:36 +0900

    Delete RecallForge/RecallForge directory

--- RecallForge/RecallForge/client/src/hooks/use-translation.ts
+++
@@ -1,9 +0,0 @@
-
-// 더 이상 사용하지 않음 - 한국어만 사용
-export function useTranslation() {
-  const t = (key: string): string => {
-    return key;
-  };
-
-  return { t };
-}
--- RecallForge/RecallForge/client/src/lib/translations.ts
+++
@@ -1,4 +0,0 @@
-
-// 더 이상 사용하지 않음 - 한국어만 사용
-export const translations = {};
-export type TranslationKey = string;
